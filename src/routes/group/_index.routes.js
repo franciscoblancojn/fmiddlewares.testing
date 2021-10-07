025ee027ed270a6c98434f8331fe401cee646ecd
franciscoblancojn <blancofrancisco34@gmail.com>
2021-10-07 09:28:45 -0500

    /group/email

--- src/routes/group/_index.routes.js
+++ src/routes/group/_index.routes.js
@@ -4,4 +4,6 @@ const router = require('express').Router()
 
 router.use('/string', require('@/routes/group/string.routes'))
 
+router.use('/email', require('@/routes/group/email.routes'))
+
 module.exports = router
---
+++ src/routes/group/email.routes.js
@@ -0,0 +1,25 @@
+require('module-alias/register')
+const router = require('express').Router()
+const fmiddlewares = require('fmiddlewares')
+const _default = require('@/controllers/default')
+
+  
+router.post(
+  '/',
+  [
+    fmiddlewares.validateItem({
+        elements:{
+            type:"group",
+            groupType:"email",
+            items:[
+                "email",
+                "email_user",
+                "email_login"
+            ],
+        }
+    })
+  ],
+  _default
+)
+
+module.exports = router
