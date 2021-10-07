3d186c18132b0aa335dd4e0b686c9df35b5fbb9f
franciscoblancojn <blancofrancisco34@gmail.com>
2021-10-06 19:56:14 -0500

    string

--- src/routes/_index.routes.js
+++ src/routes/_index.routes.js
@@ -4,4 +4,6 @@ const router = require('express').Router()
 
 router.use('/test', require('@/routes/test.routes'))
 
+router.use('/string', require('@/routes/string/string.routes'))
+
 module.exports = router
---
+++ src/routes/string/string.routes.js
@@ -0,0 +1,30 @@
+require('module-alias/register')
+const router = require('express').Router()
+const fmiddlewares = require('fmiddlewares')
+const _default = require('@/controllers/default')
+
+router.get(
+  '/', 
+  [
+    fmiddlewares.validateItem({
+      name: {
+        type: 'string',
+      },
+    },"query")
+  ],
+   _default
+)
+  
+router.post(
+  '/',
+  [
+    fmiddlewares.validateItem({
+      name: {
+        type: 'string',
+      },
+    })
+  ],
+  _default
+)
+
+module.exports = router
